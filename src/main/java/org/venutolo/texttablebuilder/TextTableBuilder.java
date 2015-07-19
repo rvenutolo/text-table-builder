2839e9c1ae818da17be4296368452366092e3dac
Rick Venutolo <rick@venutolo.org>
2015-07-19 18:00:37 -0400

    using Lists instead of arrays in TextTableBuilder

--- src/main/java/org/venutolo/texttablebuilder/TextTableBuilder.java
+++ src/main/java/org/venutolo/texttablebuilder/TextTableBuilder.java
@@ -1,5 +1,6 @@
 package org.venutolo.texttablebuilder;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,21 +11,12 @@ public class TextTableBuilder {
 
     private BoxDrawingCharacters boxDrawingCharacters = BoxDrawingCharacters.LIGHT;
 
-    private Alignment[] headerAlignments;
+    private List<Alignment> headerAlignments;
 
-    private Alignment[] rowAlignments;
+    private List<Alignment> rowAlignments;
 
-    private static <T> T[] copyArray(final T... array) {
-        // TODO replace with own implementation if want to be 1.5 compatible
-        return Arrays.copyOf(array, array.length);
-    }
-
-    private static Alignment[] getAlignmentArray(final List<Alignment> list) {
-        return list.toArray(new Alignment[list.size()]);
-    }
-
-    public BoxDrawingCharacters getBoxDrawingCharacters() {
-        return boxDrawingCharacters;
+    private static <T> List<T> defensiveCopy(final List<T> list) {
+        return new ArrayList<T>(list);
     }
 
     public TextTableBuilder setBoxDrawingCharacters(final BoxDrawingCharacters boxDrawingCharacters) {
@@ -32,30 +24,34 @@ public class TextTableBuilder {
         return this;
     }
 
-    public Alignment[] getHeaderAlignments() {
-        return copyArray(headerAlignments);
-    }
-
-    public TextTableBuilder setHeaderAlignments(final Alignment... headerAlignments) {
-        this.headerAlignments = copyArray(headerAlignments);
-        return this;
+    public BoxDrawingCharacters getBoxDrawingCharacters() {
+        return boxDrawingCharacters;
     }
 
     public TextTableBuilder setHeaderAlignments(final List<Alignment> headerAlignments) {
-        return setHeaderAlignments(getAlignmentArray(headerAlignments));
-    }
-
-    public Alignment[] getRowAlignments() {
-        return copyArray(rowAlignments);
-    }
-
-    public TextTableBuilder setRowAlignments(final Alignment... rowAlignments) {
-        this.rowAlignments = copyArray(rowAlignments);
+        this.headerAlignments = defensiveCopy(headerAlignments);
         return this;
     }
 
+    public TextTableBuilder setHeaderAlignments(final Alignment... headerAlignments) {
+        return setHeaderAlignments(Arrays.asList(headerAlignments));
+    }
+
+    public List<Alignment> getHeaderAlignments() {
+        return defensiveCopy(headerAlignments);
+    }
+
     public TextTableBuilder setRowAlignments(final List<Alignment> rowAlignments) {
-        return setRowAlignments(getAlignmentArray(rowAlignments));
+        this.rowAlignments = defensiveCopy(rowAlignments);
+        return this;
+    }
+
+    public TextTableBuilder setRowAlignments(final Alignment... rowAlignments) {
+        return setRowAlignments(Arrays.asList(rowAlignments));
+    }
+
+    public List<Alignment> getRowAlignments() {
+        return defensiveCopy(rowAlignments);
     }
 
 }
