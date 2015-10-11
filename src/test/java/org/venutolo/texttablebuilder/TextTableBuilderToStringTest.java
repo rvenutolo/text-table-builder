3bb4ad9875577d3ab745eaf18a81e50d19015e40
Rick Venutolo <rick@venutolo.org>
2015-10-11 17:43:53 -0400

    Header alignment/toString unit tests

--- src/test/java/org/venutolo/texttablebuilder/TextTableBuilderToStringTest.java
+++ src/test/java/org/venutolo/texttablebuilder/TextTableBuilderToStringTest.java
@@ -1,5 +1,6 @@
 package org.venutolo.texttablebuilder;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,16 +21,101 @@ public class TextTableBuilderToStringTest {
                 .setBoxDrawingCharacters(BoxDrawingCharacters.forCharacter('#'));
     }
 
+    private static String joinForTable(final String... strings) {
+        return StringUtils.join(strings, "\n");
+    }
+
+    /*========================================================================
+     * HEADER ALIGNMENT - TOSTRING TESTS
+     *========================================================================*/
+
+    @Test
+    public void testHeaderAlignmentsWhenNotSetToString() {
+        textTableBuilder.setHeaders("h", "h", "h");
+        textTableBuilder.addRow("aaa", "bbb", "ccc");
+        // headers should be left-aligned when alignment has not been set
+        final String expected = joinForTable(
+                "###################",
+                "# h   # h   # h   #",
+                "###################",
+                "# aaa # bbb # ccc #",
+                "###################"
+        );
+        assertEquals(
+                "toString() did not produced expected value",
+                expected,
+                textTableBuilder.toString()
+        );
+    }
+
+    @Test
+    public void testHeaderAlignmentsWhenSetToString() {
+        textTableBuilder.setHeaders("h", "h", "h");
+        textTableBuilder.addRow("aaa", "bbb", "ccc");
+        textTableBuilder.setHeaderAlignments(LEFT, LEFT, LEFT);
+        final String leftExpected = joinForTable(
+                "###################",
+                "# h   # h   # h   #",
+                "###################",
+                "# aaa # bbb # ccc #",
+                "###################"
+        );
+        assertEquals(
+                "toString() did not produced expected value",
+                leftExpected,
+                textTableBuilder.toString()
+        );
+        textTableBuilder.setHeaderAlignments(RIGHT, RIGHT, RIGHT);
+        final String rightExpected = joinForTable(
+                "###################",
+                "#   h #   h #   h #",
+                "###################",
+                "# aaa # bbb # ccc #",
+                "###################"
+        );
+        assertEquals(
+                "toString() did not produced expected value",
+                rightExpected,
+                textTableBuilder.toString()
+        );
+    }
+
+    @Test
+    public void testClearHeaderAlignmentsToString() {
+        textTableBuilder.setHeaders("h", "h", "h");
+        textTableBuilder.addRow("aaa", "bbb", "ccc");
+        textTableBuilder.setHeaderAlignments(RIGHT, RIGHT, RIGHT);
+        textTableBuilder.clearHeaderAlignments();
+        // headers should be left-aligned when alignment has not been set
+        final String expected = joinForTable(
+                "###################",
+                "# h   # h   # h   #",
+                "###################",
+                "# aaa # bbb # ccc #",
+                "###################"
+        );
+        assertEquals(
+                "toString() did not produced expected value",
+                expected,
+                textTableBuilder.toString()
+        );
+    }
+
+    /*========================================================================
+     * COLUMN ALIGNMENT - TOSTRING TESTS
+     *========================================================================*/
+
     @Test
     public void testColumnAlignmentsWhenNotSetToString() {
         textTableBuilder.addRow("a", "bbb", "cc");
         textTableBuilder.addRow("dddd", "ee", "f");
         // columns should be left-aligned when alignment has not been set
-        final String expected = ""
-                                + "###################\n"
-                                + "# a    # bbb # cc #\n"
-                                + "# dddd # ee  # f  #\n"
-                                + "###################";
+        final String expected = joinForTable(
+                "###################",
+                "# a    # bbb # cc #",
+                "# dddd # ee  # f  #",
+                "###################"
+        );
         assertEquals(
                 "toString() did not produced expected value",
                 expected,
@@ -42,22 +128,24 @@ public class TextTableBuilderToStringTest {
         textTableBuilder.addRow("a", "bbb", "cc");
         textTableBuilder.addRow("dddd", "ee", "f");
         textTableBuilder.setColumnAlignments(LEFT, LEFT, LEFT);
-        final String leftExpected = ""
-                                    + "###################\n"
-                                    + "# a    # bbb # cc #\n"
-                                    + "# dddd # ee  # f  #\n"
-                                    + "###################";
+        final String leftExpected = joinForTable(
+                "###################",
+                "# a    # bbb # cc #",
+                "# dddd # ee  # f  #",
+                "###################"
+        );
         assertEquals(
                 "toString() did not produced expected value",
                 leftExpected,
                 textTableBuilder.toString()
         );
         textTableBuilder.setColumnAlignments(RIGHT, RIGHT, RIGHT);
-        final String rightExpected = ""
-                                     + "###################\n"
-                                     + "#    a # bbb # cc #\n"
-                                     + "# dddd #  ee #  f #\n"
-                                     + "###################";
+        final String rightExpected = joinForTable(
+                "###################",
+                "#    a # bbb # cc #",
+                "# dddd #  ee #  f #",
+                "###################"
+        );
         assertEquals(
                 "toString() did not produced expected value",
                 rightExpected,
@@ -71,11 +159,13 @@ public class TextTableBuilderToStringTest {
         textTableBuilder.addRow("dddd", "ee", "f");
         textTableBuilder.setColumnAlignments(RIGHT, RIGHT, RIGHT);
         textTableBuilder.clearColumnAlignments();
-        final String expected = ""
-                                + "###################\n"
-                                + "# a    # bbb # cc #\n"
-                                + "# dddd # ee  # f  #\n"
-                                + "###################";
+        // columns should be left-aligned when alignment has not been set
+        final String expected = joinForTable(
+                "###################",
+                "# a    # bbb # cc #",
+                "# dddd # ee  # f  #",
+                "###################"
+        );
         assertEquals(
                 "toString() did not produced expected value",
                 expected,
